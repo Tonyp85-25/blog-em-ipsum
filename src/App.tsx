906add9ed198f7b6423109408be729ca6a93c5c1
Tonyp85-25 <anthony.paulin@enil-mamirolle.fr>
2023-05-29 10:50:42 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Posts } from './Posts';
+
+function App() {
+ 
+  return (
+    // provide React Query client to App
+    <div className="App">
+      <h1>Blog Posts</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App
