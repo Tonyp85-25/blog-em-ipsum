e5b76e16a7076aeb1ede1ccd01c7b273f5bc3db4
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2023-05-31 22:48:10 +0200

    add comments fetching

--- src/PostDetail.tsx
+++ src/PostDetail.tsx
@@ -1,51 +1,64 @@
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { Post } from "./types";
 
-async function fetchComments(postId:number) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-    );
-    return response.json();
-  }
-  
-  async function deletePost(postId:number) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/postId/${postId}`,
-      { method: "DELETE" }
-    );
-    return response.json();
-  }
-  
-  async function updatePost(postId:number) {
-    const response = await fetch(
-      `https://jsonplaceholder.typicode.com/postId/${postId}`,
-      { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-    );
-    return response.json();
-  }
+async function fetchComments(postId: number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
+  );
+  return response.json();
+}
 
+async function deletePost(postId: number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "DELETE" }
+  );
+  return response.json();
+}
 
+async function updatePost(postId: number) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/postId/${postId}`,
+    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+  );
+  return response.json();
+}
 
-  interface Comment{
-    id:number;
-    email:string;
-    body:string;
+interface Comment {
+  id: number;
+  email: string;
+  body: string;
+}
+
+export function PostDetail({ post }: { post: Post }) {
+  const { data, isLoading, isError, error }: UseQueryResult<Comment[], Error> =
+    useQuery({
+      queryKey: ["comments", post.id],
+      queryFn: () => fetchComments(post.id),
+    });
+  if (isLoading) {
+    return <h3>Loading...</h3>;
   }
-  
-  export function PostDetail({ post }:{post:Post}) {
-    // replace with useQuery
-    const data :Comment[] = [];
-  
+  if (isError) {
     return (
       <>
-        <h3 style={{ color: "blue" }}>{post.title}</h3>
-        <button>Delete</button> <button>Update title</button>
-        <p>{post.body}</p>
-        <h4>Comments</h4>
-        {data.map((comment) => (
-          <li key={comment.id}>
-            {comment.email}: {comment.body}
-          </li>
-        ))}
+        <h3>Oops! Something went wrong!</h3>
+        <p>{error.message}</p>
       </>
     );
   }
+
+  return (
+    <>
+      <h3 style={{ color: "blue" }}>{post.title}</h3>
+      <button>Delete</button> <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
