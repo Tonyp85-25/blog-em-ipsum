9cdb8263ec5902044714deabc4037b62255a1f9d
Tonyp85-25 <anthony-paulin@etudiant.oclock.io>
2023-06-03 15:16:57 +0200

    adds delete and upadate mutations and fix tsc error

--- src/PostDetail.tsx
+++ src/PostDetail.tsx
@@ -1,4 +1,4 @@
-import { UseQueryResult, useQuery } from "@tanstack/react-query";
+import { UseQueryResult, useMutation, useQuery } from "@tanstack/react-query";
 import { Post } from "./types";
 
 async function fetchComments(postId: number) {
@@ -19,7 +19,7 @@ async function deletePost(postId: number) {
 async function updatePost(postId: number) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    { method: "PATCH", body: '{ title: "REACT QUERY FOREVER!!!!" }' }
   );
   return response.json();
 }
@@ -36,6 +36,15 @@ export function PostDetail({ post }: { post: Post }) {
       queryKey: ["comments", post.id],
       queryFn: () => fetchComments(post.id),
     });
+
+  const deleteMutation = useMutation<Comment[], Error, number>({
+    mutationFn: (postId: number) => deletePost(postId),
+  });
+
+  const updateMutation = useMutation<Comment[], Error, number>({
+    mutationFn: (postId: number) => updatePost(postId),
+  });
+
   if (isLoading) {
     return <h3>Loading...</h3>;
   }
@@ -51,7 +60,29 @@ export function PostDetail({ post }: { post: Post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting...</p>
+      )}
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>{deleteMutation.error.message}</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>(not)Deleted!</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isLoading && (
+        <p style={{ color: "purple" }}>Updating...</p>
+      )}
+      {updateMutation.isError && (
+        <p style={{ color: "red" }}>{updateMutation.error.message}</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: "green" }}>(not)Updated!</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
